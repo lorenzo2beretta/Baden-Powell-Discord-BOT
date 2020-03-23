5b6940b0131b5091c803a3dec9a083f25c320ddf
lorenzo2beretta <lorenzo2beretta@gmail.com>
2020-03-23 04:19:27 +0100

    chat segreta e gif handleer

--- working_bot.py
+++ working_bot.py
@@ -1,29 +1,40 @@
 import random
+import schedule
+from sys import argv
 import discord
 from discord.ext import commands, tasks
 
-TOKEN = 'NjkxMDIyNjg0NDk0MTAyNTM5.XneIUQ.SIc7M9-BSZ2ySk8KRyim-EeejJM'
-GENERAL_CHAT = 691024389730336842
+with open('token', 'r') as file:
+    TOKEN = file.read()
+
+DEBUG_CHAT = 691024389730336842
+REPARTO_CHAT = 690344893675339962
+GENERAL_CHAT = DEBUG_CHAT if len(argv) > 1 else REPARTO_CHAT
 bot = commands.Bot(command_prefix=['bp ', 'BP ', 'B.P. ', 'b.p. '])
 
 @bot.event
 async def on_ready():
     print('Bot is Ready.')
-
+    
+@tasks.loop(hours=24, count=5)
+async def count():
+    await bot.wait_until_ready()
+    channel = bot.get_channel(GENERAL_CHAT)
+    await channel.send('Conto!')
 
 # ------------------------------ BP CIAO o SILENZIO -----------------------
+scout_sign_picture = discord.File('scout_sign.jpg')
+
 @bot.command(aliases=['silenzio', 'ciao'])
 async def saluto(ctx):
-    channel = ctx.channel
-    picture = discord.File('scout_sign.jpg')
-    await channel.send(file=picture)
+    channel = ctx.channel    
+    await channel.send(file=scout_sign_picture)
 
 
 # ------------------------------ CITAZIONI BP -----------------------------
-bp_quotes_file = open('bp_quotes.txt', 'r')
-quotes = bp_quotes_file.readlines()
-quotes = ['"' + quote[:-1] + '"' for quote in quotes]
-bp_quotes_file.close()
+with open('bp_quotes.txt', 'r') as file:
+    quotes = file.readlines()
+    quotes = ['"' + quote[:-1] + '"' for quote in quotes]
 
 @bot.command(aliases=['cit', 'quote', 'frase', 'cit.'])
 async def citazione(ctx):
@@ -34,10 +45,10 @@ async def citazione(ctx):
     await channel.send(intro + quote)
 
 # --------------------------- RIMPROVERO PAROLACCE --------------------------
-bad_words_file = open('bad_words.txt', 'r')
-content = bad_words_file.read()
-bad_words = sorted(content.split(), key=lambda s: len(s), reverse=True)
-bad_words_file.close()
+with open('bad_words.txt', 'r') as file:
+    content = file.read()
+    bad_words = sorted(content.split(), key=lambda s: len(s), reverse=True)
+    
 
 @bot.event
 async def on_message(message):
@@ -60,5 +71,6 @@ async def on_message(message):
             await channel.send(answer)
             break
 
+# count.start()
 bot.run(TOKEN)
 
